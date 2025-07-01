a4fbdbfe0079c2c0dd409daf879cb08549a374ff
Bilal Benyoussef <bilalbeny735@gmail.com>
2025-07-01 14:07:06 +0100

    feat: configure React Query and create user mutation hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './lib/AppRoutes';
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+
+    }
+  }
+});
 
 export default function App() {
   return (
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   )
 }
---
+++ frontend/src/api/MyUserApi.tsx
@@ -0,0 +1,14 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+
+type CreateUserRequest = {
+    auth0Id: string;
+    email: string
+}
+
+
+export const useCreateMyUser = () => {
+    const createMyUser = async () => {
+
+    }
+}
